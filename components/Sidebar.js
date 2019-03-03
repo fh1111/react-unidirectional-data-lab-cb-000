3d03391b92b517d9c0ad2d23967f9322bdd6ee44
Fer Hernandez <fh.1111@gmail.com>
2019-03-03 20:48:12 +0000

    Automatically backed up by Learn

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,7 +4,17 @@ import React from 'react';
 import SidebarItem from './SidebarItem';
 
 export default class Sidebar extends React.Component {
+
+
+    constructor(props) {
+      super(props);
+      this.handleClick = this.handleClick.bind(this);
+    }
+
+
   handleClick(index, ev) {
+    ev.preventDefault();
+    this.props.onSelect(index)    
   }
   render() {
     const { files, selectedFileIndex, onAdd } = this.props;
